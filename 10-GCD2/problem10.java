33e9a382bcba7265870c19b2e79234ff3630fccb
praveen <praveenpalavalasa007@gmail.com>
2023-08-02 21:16:10 +0530

    third commit

--- 10-GCD2/problem10.java
+++ 10-GCD2/problem10.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class problem{
+class problem10{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("enter value of a:");
--- 7-fibanocci/problem7.java
+++ 7-fibanocci/problem7.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class problem{
+class problem7{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("enter the nth value:");
